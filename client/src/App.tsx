8e723bf4301675800a3c41ecdae01c786e806a62
GabrielSouzaCosta <gabrielsscosta2010@hotmail.com>
2022-07-20 15:31:08 -0300

    details page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home'
+import ProductDetails from "./pages/ProductDetails";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={ <Home /> }/>
+        <Route path='/shoes/:id' element={ <ProductDetails /> } />
+        <Route path='/shoes' element={ <Products /> } />
       </Routes>
     </BrowserRouter>
   )
