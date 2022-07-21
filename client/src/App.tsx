b33e80a38f51dbc71a5453d1b17266f413a358db
GabrielSouzaCosta <gabrielsscosta2010@hotmail.com>
2022-07-21 08:57:06 -0300

    navigation bar links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home'
+import LoginPage from "./pages/LoginPage";
 import ProductDetails from "./pages/ProductDetails";
 import Products from "./pages/Products";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path='/' element={ <Home /> }/>
         <Route path='/shoes/:id' element={ <ProductDetails /> } />
         <Route path='/shoes' element={ <Products /> } />
+        <Route path='/login' element={ <LoginPage /> } />
+        <Route path='/register' element={ <RegisterPage /> } />
       </Routes>
     </BrowserRouter>
   )
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,15 +1,25 @@
-import { Container, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <Navbar bg="dark" expand="lg" className="sticky-top">
-      <Container>
-        <Navbar.Brand as="h1" className="text-white m-0 fs-1 brand mx-auto">
-          <Link to="/">
-            Breathe Shoes
-          </Link>
-        </Navbar.Brand>
+    <Navbar bg="dark" expand="lg" className="sticky-top justify-content-center">
+      <Container fluid>
+        <Nav className='justify-content-center align-items-center w-100 fs-4 text-uppercase'>
+          <Navbar.Brand as="h1" className="text-white m-0 fs-1 brand">
+            <NavLink to="/" className="custom-brand-link">
+              Breathe Shoes
+            </NavLink>
+          </Navbar.Brand>
+          <NavLink to="/shoes" className={({ isActive }) =>
+              isActive ? "text-danger ps-3 navlink-active" : "text-danger ps-2 line-down"
+            } >
+            Store
+          </NavLink>
+          <NavLink to="/login" className="line-down position-absolute end-0 pe-3">
+            Login
+          </NavLink>
+        </Nav>
       </Container>
     </Navbar>
   )
